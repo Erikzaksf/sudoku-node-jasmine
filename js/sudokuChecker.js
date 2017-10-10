c04e796b1bd282d7fa0f0edaf1b301f0e6bfe28b
Erik Zakrzewski and Kristen Kulha <kristen@>
2017-10-10 15:05:20 -0700

    Add constructor and method to pass first spec.

--- js/sudokuChecker.js
+++ js/sudokuChecker.js
@@ -1,6 +1,15 @@
+function SudokuChecker(){}
 
-
-
+SudokuChecker.prototype.validRow = function(row) {
+  var userRow = row;
+  for(i = 1; i <= userRow.length; i++) {
+    if (userRow.includes(i)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
 
 
--- spec/sudokuChecker-spec.js
+++ spec/sudokuChecker-spec.js
@@ -1,13 +1,13 @@
-var sudokuChecker  = require('./../js/sudokuChecker.js').sudokuCheckerModule;
+var SudokuChecker  = require('./../js/sudokuChecker.js').sudokuCheckerModule;
 
 describe("SudokuChecker", function(){
   var testChecker;
 
   beforeEach(function() {
-    testChecker = new sudokuChecker();
+    testChecker = new SudokuChecker();
   });
 
-  it('will return true if a single row containts numbers 1-9 with no repeats', function() {
+  it('will return true if a single row contains numbers 1-9 with no repeats', function() {
     expect(testChecker.validRow([2,3,1,5,4,7,9,6,8])).toEqual(true);
   });
 
